fd8f63888a30437963bb26fc4a989eeaff226590
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2023-02-14 13:37:41 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+public class Add {
+    // static  void sum(int... a){
+
+    // }
+    // static void sum2(int []a){
+
+    // }
+    static public  void main(String ...arguments) {
+    //   sum(10,20,30,340,40,50);  
+    //   int r[] = new int[10];
+    //   sum2(r);
+    }
+}
+
+// Add 100 200 300 40 50
---
+++ FirstClass.java
@@ -0,0 +1,22 @@
+// Java is OOPS
+// class is a keyword in java, keywords (small letters)
+// class SecondClass{
+
+// }
+import static java.lang.System.out;
+public class FirstClass{
+    // nothing return - void
+    // static - keyword - when class is loaded in the memory 
+    //, static things will be loaded along
+    // public - access outside the folder/package and also inside
+    // String args[] = Command Line Arguments
+    // args = name of arguments
+    // String - Collection of chars (Internally predefine class)
+    // [] array
+    public static void main(String arg[]){
+        //System s = new System();
+        out.println("Hello Java");
+        out.println("Hello Java");
+        
+    }
+}
